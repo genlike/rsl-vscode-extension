98a95674b73457c510b14dc3de64197fb90b5397
genlike <ricardo.f.oliveira.da.silva@tecnico.ulisboa.pt>
2023-02-21 05:09:18 +0000

    run only asl

--- src/rsl-extension.ts
+++ src/rsl-extension.ts
@@ -17,13 +17,13 @@
 
 
 import * as vscode from 'vscode';
-import { RSLLspVscodeExtension } from './rsl-lsp-extension';
+// import { RSLLspVscodeExtension } from './rsl-lsp-extension';
 import { SprottyLspVscodeExtension } from 'sprotty-vscode/lib/lsp';
 
 let extension: SprottyLspVscodeExtension;
 
 export function activate(context: vscode.ExtensionContext) {
-    extension = new RSLLspVscodeExtension(context);
+    //extension = new RSLLspVscodeExtension(context);
 }
 
 export function deactivate(): Thenable<void> {
